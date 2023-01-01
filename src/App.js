ee0409be50af289ecbd5c8e3d84c4fd5df0e7845
seanreid1997 <reid.sean14@yahoo.com>
2023-01-01 21:46:26 +0000

    Add base components for authentification and default pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './App.css';
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div>
+      <HomePage />
       <Container>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/" render={() => <h1>home</h1>} />
+          <Route exact path="/login" render={() => <h1>login</h1>} />
+          <Route exact path="/signup" render={() => <h1>signup</h1>} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,27 @@
-import './App.css';
+
 import React, { Component } from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/logo.jpg'
 
 export class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>HomePage</h1>
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="50" border-radius="25" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link >Home</Nav.Link>
+            <Nav.Link >Login</Nav.Link>
+            <Nav.Link >Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </div>
     )
   }
